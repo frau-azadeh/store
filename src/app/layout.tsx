f95a6c443fb9e5003adaffcde8d6d6cbbde2ffcd
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-13 07:51:37 -0800

    update Navbar prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="fa" dir="rtl">
       <body>
-        <Navbar/>
+        <Navbar />
         {children}
       </body>
     </html>
