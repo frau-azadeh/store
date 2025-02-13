cb57f3ad2632bcadfadfd20043049e2cba3f8c84
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-13 07:51:08 -0800

    add Navbar components

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,8 @@
+export default function Navbar(){
+    return(
+     <header className="bg-white shadow-md p-4 flex justify-between items-center">
+           <h3 className="text-lx font-bold text-orange-600">فروشگاه کفش</h3>
+            <button className="bg-blue-800 text-white p-2 rounded-lg">ورود / ثبت نام</button>
+     </header>
+    )
+}
