0ce9ad081e3524c7a4c63bf3d003e28530348a74
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-13 06:35:32 -0800

    rename folder components

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Product } from "@/types/products";
+import Image from "next/image";
+import Button from "./Button";
+import { ShoppingCart } from "lucide-react";
+
+interface ProductCardProps {
+  product: Product;
+}
+
+export default function ProductCard({ product }: ProductCardProps) {
+  const { id, name, price, image, category, gender, sizes, colors } = product;
+
+  return (
+    <div className="border rounded-lg p-4 bg-white shadow-md hover:shadow-lg transition-shadow">
+      <Image
+        src={image}
+        alt={name}
+        width={300}
+        height={200}
+        className="w-full h-48 object-cover rounded-md mb-4"
+      />
+      <h3 className="text-lg font-bold">{name}</h3>
+      <p className="text-gray-600 text-sm">
+        {category} - {gender}
+      </p>
+      <p className="text-gray-500 text-sm">سایزها: {sizes.join(", ")}</p>
+      <p className="text-gray-500 text-sm">رنگ ها: {colors.join(", ")}</p>
+      <p className="text-blue-500 font-bold">{price} تومان</p>
+      <Button
+        onClick={() => console.log("افزودن به سبد")}
+        icon={<ShoppingCart size={18} />}
+        className="bg-blue-500 text-white hover:bg-blue-600"
+      >
+        افزودن به سبد خرید
+      </Button>
+    </div>
+  );
+}
