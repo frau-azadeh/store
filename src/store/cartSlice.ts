0f5f94f4cb6e05dff7751e80572621365c55d445
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-14 07:35:42 -0800

    add local storage

--- src/store/cartSlice.ts
+++ src/store/cartSlice.ts
@@ -1,53 +1,17 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { Product } from "@/types/products";
 
-// خواندن از localStorage وقتی صفحه لود میشه
-const loadCartFromStorage = () => {
-  if (typeof window !== "undefined") {
-    const storedCart = localStorage.getItem("cart");
-    return storedCart ? JSON.parse(storedCart) : [];
-  }
-  return [];
-};
-
-interface CartItem extends Product {
-  quantity: number;
+const loadCartFromStorage = () =>{
+    if(typeof window !== "undefined"){
+        const storeCart = localStorage.getItem("cart");
+        return storeCart? JSON.parse(storeCart) : [];
+    }
 }
 
-interface CartState {
-  items: CartItem[];
+interface CartItem extends Product{
+    quantity: number;
 }
 
-const initialState: CartState = {
-  items: loadCartFromStorage(),
-};
+interface CartState{
+    items: CartItem[];
+}
 
-const cartSlice = createSlice({
-  name: "cart",
-  initialState,
-  reducers: {
-    addToCart: (state, action: PayloadAction<Product>) => {
-      const existingItem = state.items.find((item) => item.id === action.payload.id);
-      if (existingItem) {
-        existingItem.quantity += 1;
-      } else {
-        state.items.push({ ...action.payload, quantity: 1 });
-      }
-
-      localStorage.setItem("cart", JSON.stringify(state.items));
-    },
-
-    removeFromCart: (state, action: PayloadAction<number>) => {
-      state.items = state.items.filter((item) => item.id !== action.payload);
-      localStorage.setItem("cart", JSON.stringify(state.items));
-    },
-
-    clearCart: (state) => {
-      state.items = [];
-      localStorage.removeItem("cart");
-    },
-  },
-});
-
-export const { addToCart, removeFromCart, clearCart } = cartSlice.actions;
-export default cartSlice.reducer;
