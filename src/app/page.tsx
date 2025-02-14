dab7c0039f40a8808ff76ef36f29dd647b0802e7
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-14 06:56:25 -0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/Navbar";
 import ProductCard from "@/components/ProductCard";
 import productsData from "@/data/products.json";
 import { Product } from "@/types/products";
@@ -6,10 +7,14 @@ export default function HomePage() {
   const products = productsData as Product[];
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 py-8">
+    <div>
+      <Navbar/>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 py-8">
       {products.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
     </div>
+    </div>
+    
   );
 }
