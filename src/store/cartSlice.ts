a56f225255c7d025f1adac3f63805eb180247687
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-14 07:44:36 -0800

    add reducers

--- src/store/cartSlice.ts
+++ src/store/cartSlice.ts
@@ -1,17 +1,32 @@
 import { Product } from "@/types/products";
+import { createSlice } from "@reduxjs/toolkit";
 
 const loadCartFromStorage = () =>{
-    if(typeof window !== "undefined"){
+    if (typeof window !== "undefined"){
         const storeCart = localStorage.getItem("cart");
-        return storeCart? JSON.parse(storeCart) : [];
+        return storeCart ? JSON.parse(storeCart) : [];
     }
+    return [];
 }
 
 interface CartItem extends Product{
     quantity: number;
 }
 
-interface CartState{
+interface CartState {
     items: CartItem[];
 }
+
+const initialState: CartState = {
+    items: loadCartFromStorage(),
+};
+
+const cartSlice = createSlice({
+    name: "cart",
+    initialState,
+    reducers:{
+        
+    }
+})
+
 
