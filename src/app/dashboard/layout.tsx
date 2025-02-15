219464ab256d788ee5a615029cd7d889e5c73f32
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-15 09:00:15 -0800

    update

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,18 @@
+import Sidebar from "@/components/dashboard/Sidebar";
+import Header from "@/components/dashboard/Header";
+
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex flex-col md:flex-row h-screen bg-gray-50">
+      {/* Sidebar فقط در دسکتاپ */}
+      <div className="hidden md:block">
+        <Sidebar />
+      </div>
+
+      <div className="flex-1 flex flex-col">
+        <Header />
+        <main className="p-4">{children}</main>
+      </div>
+    </div>
+  );
+}
