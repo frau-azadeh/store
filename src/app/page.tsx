bcc2921d3bc49549638dfab92473c421f552a404
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-06-01 17:47:13 +0330

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Categories from "@/components/Categories";
 import FeaturedProducts from "@/components/FeaturedProducts";
