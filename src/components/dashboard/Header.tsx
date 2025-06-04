9e7bd6eb0ebda8af48c7390a3bcff20b1d2df02a
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-06-04 12:26:02 +0330

    update

--- src/components/dashboard/Header.tsx
+++ src/components/dashboard/Header.tsx
@@ -1,22 +1,13 @@
 "use client";
 
-import { Menu } from "lucide-react";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import Sidebar from "./Sidebar";
+
 
 export default function Header() {
   return (
     <div className="bg-white p-4 shadow flex justify-between items-center">
       {/* موبایل منو */}
       <div className="md:hidden">
-        <Sheet>
-          <SheetTrigger>
-            <Menu />
-          </SheetTrigger>
-          <SheetContent side="left" className="w-64">
-            <Sidebar />
-          </SheetContent>
-        </Sheet>
+        
       </div>
 
       <h1 className="text-lg font-semibold hidden md:block">داشبورد مدیریت</h1>
