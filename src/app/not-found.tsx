22ce3e950d0ab839305851b57fba86bb2146ba65
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-06-04 18:46:14 +0330

    add 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function NotFound(){
+    return(
+        <div className="flex flex-col h-screen mx-auto justify-center items-center">
+            <Image
+                width={400}
+                height={400}
+                alt="not found page"
+                src="/images/not-found.png"
+                className="rounded-lg shadow-md hover:scale-105 transition duration-300"
+            />
+        </div>
+    )
+}
