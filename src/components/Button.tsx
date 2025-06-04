a07d02729c2fe976e2164b3287299215741a4f3f
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-06-04 19:49:45 +0330

    update Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,27 +1,41 @@
-"use client";
+import clsx from 'clsx';
+import React, { ButtonHTMLAttributes, ReactNode } from 'react'
 
-import { ReactNode } from "react";
-
-interface ButtonProps {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
+  variant?: "primary" | "secondary" | "danger" | "call";
+  size?: "xs" | "md" | "lg";
   children: ReactNode;
-  onClick?: () => void;
-  icon?: ReactNode;
-  className?: string;
 }
-
-export default function Button({
+const Button: React.FC<ButtonProps> = ({
+  className,
+  variant = "primary",
+  size = "md",
   children,
-  onClick,
-  icon,
-  className = "",
-}: ButtonProps) {
+  ...props
+}) => {
+  const variantClass = {
+    primary: "bg-blue-500 hover:bg-blue-700",
+    secondary: "bg-gray-500 hover:bg-gray-700",
+    danger: "bg-red-500 hover:bg-red-700",
+    call: "bg-green-500 hover:bg-green-700"
+  }
+  const sizeClass = {
+    xs: "px-3 py-2 text-xs",
+    md: "px-4 py-3 text-base",
+    lg: "px-6 py-4 text-lg",
+  }
   return (
     <button
-      onClick={onClick}
-      className={`flex items-center justify-center gap-2 px-4 py-2 rounded-lg transition ${className}`}
+      {...props}
+      className={clsx(
+        "rounded text-white duration-300 transition",
+        variantClass[variant], sizeClass[size],
+        className
+      )}
     >
-      {icon && <span>{icon}</span>}
       {children}
     </button>
-  );
+  )
 }
+
+export default Button
