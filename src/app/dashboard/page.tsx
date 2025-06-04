9c34d8caa46c0d7e8d9ac9fddd37f65ff7a81c27
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-06-04 12:58:54 +0330

    update prettier

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,3 @@
-
 export default function DashboardPage() {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4"></div>
--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,22 +1,7 @@
-
-
-
-
-export default function ProductDetailPage(){
- 
+export default function ProductDetailPage() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-10 p-8 bg-white shadow-lg rounded-xl">
       {/* تصویر */}
-
-       
-      </div>
-
-    
-
-
-        
-    
-
-
+    </div>
   );
 }
