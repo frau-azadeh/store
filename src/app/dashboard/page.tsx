ec8a7471ceb080b9884fbcf9381894d03516840e
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-06-04 15:19:38 +0330

    update

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 export default function DashboardPage() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4"></div>
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 h-screen"></div>
   );
 }
