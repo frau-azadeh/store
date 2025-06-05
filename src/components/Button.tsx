0efb9bcc22907b9f9f20daef4d5fa38094d1a55d
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-06-05 12:36:56 +0330

    add icon in Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,20 @@
 import clsx from "clsx";
 import React, { ButtonHTMLAttributes, ReactNode } from "react";
+import { LucideIcon } from "lucide-react";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: "primary" | "secondary" | "danger" | "call";
   size?: "xs" | "md" | "lg";
   children: ReactNode;
+  icon?: LucideIcon; 
 }
+
 const Button: React.FC<ButtonProps> = ({
   className,
   variant = "primary",
   size = "md",
   children,
+  icon: Icon, 
   ...props
 }) => {
   const variantClass = {
@@ -19,21 +23,24 @@ const Button: React.FC<ButtonProps> = ({
     danger: "bg-red-500 hover:bg-red-700",
     call: "bg-green-500 hover:bg-green-700",
   };
+
   const sizeClass = {
     xs: "px-3 py-2 text-xs",
     md: "px-4 py-3 text-base",
     lg: "px-6 py-4 text-lg",
   };
+
   return (
     <button
       {...props}
       className={clsx(
-        "rounded text-white duration-300 transition",
+        "rounded text-white duration-300 transition inline-flex items-center gap-2", // 👈 فاصله بین آیکن و متن
         variantClass[variant],
         sizeClass[size],
         className,
       )}
     >
+      {Icon && <Icon className="w-4 h-4" />} 
       {children}
     </button>
   );
